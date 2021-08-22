c34128f998cbbd42b54d957f7b4ba2af87a59fe2
jiaqixuac <jiaqixuac@gmail.com>
2021-08-22 15:32:14 +0800

    Update run files

--- run/herdemo_needlepick.sh
+++ run/herdemo_needlepick.sh
@@ -3,5 +3,4 @@ python -m baselines.run \
 --demo_file=../surrol/data/demo/data_NeedlePick-v0_random_100.npz \
 --save_path=../policies/her/NeedlePick-demo1e5_0 \
 --bc_loss=1 --q_filter=1 --num_demo=100 --demo_batch_size=128 --prm_loss_weight=0.001 --aux_loss_weight=0.0078 --n_cycles=20 --batch_size=1024 --random_eps=0.1 --noise_eps=0.1 \
---log_path=../logs/her/NeedlePick-demo1e5_0 \
---save_video_interval=20
+--log_path=../logs/her/NeedlePick-demo1e5_0
