8ee12565d7bb8c0a181444cbd6ebcc3d85f49796
LONG Yonghao <72002268+YonghaoLong@users.noreply.github.com>
2023-06-30 19:36:56 +0800

    Update setup_haptic.sh
    bug fixing

--- setup_haptic.sh
+++ setup_haptic.sh
@@ -7,4 +7,4 @@ cd $SRC_DIR
 swig -python touch_haptic.i
 python setup.py build_ext
 cd ..
-cp $SRC_DIR/build/lib.*/_touch_haptic.*.so $SRC_DIR/_touch_haptic.so
+cp $SRC_DIR/build/lib.*/_touch_haptic.*.so $SRC_DIR
